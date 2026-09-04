6be9f5293fe1ef3df85c692ebfe188b73a2ea0d5
umeshbuilds <umenariya836@gmail.com>
2026-09-04 21:34:38 +0530

    merge sort using recursion

---
+++ src/MergeSortRecursion.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class MergeSortRecursion {
+    static void main(String[] args) {
+     int[] arr = {5,4,3,2,1};
+     arr = mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static int[] mergeSort(int[] arr){
+        if(arr.length == 1){
+            return arr;
+        }
+        int mid = arr.length/2;
+        int[] left = mergeSort(Arrays.copyOfRange(arr,0,mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr,mid,arr.length));
+        return merge(left,right);
+    }
+    static int[] merge(int[] first,int[] second){
+     int[] mix = new int[first.length+ second.length];
+      int i = 0;
+      int j = 0;
+      int k = 0;
+
+      while (i< first.length && j< second.length){
+          if(first[i] < second[i]){
+              mix[k] = first[i];
+              i++;
+          }else{
+              mix[k] = second[j];
+              j++;
+          }
+          k++;
+      }
+      //it may be possible that one of the array is not complete
+        //copy the remaining element
+        while(i< first.length){
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+        while(j< first.length){
+            mix[k] = first[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+}
