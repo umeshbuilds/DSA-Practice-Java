44802f2c5fe6a4feeade93a3176d25fd6f1d4e8d
umeshbuilds <umenariya836@gmail.com>
2026-09-13 23:59:38 +0530

    String subSequence pattern concept

--- src/StringRecursion/SkipCharacter.java
+++ src/StringRecursion/SkipCharacter.java
@@ -3,6 +3,8 @@ package stringRecursion;
 public class SkipCharacter {
     public static void main(String[] args) {
      skip("","baccdah");
+     String ans = skipWord("helloappleUmesh");
+        System.out.println(ans);
     }
     static void skip(String p, String up){
         if(up.isEmpty()){
@@ -14,6 +16,17 @@ public class SkipCharacter {
             skip(p,up.substring(1));
         }else{
             skip(p+ch,up.substring(1));
+        }
+    }
+
+    static String skipWord(String word){
+        if(word.isEmpty()){
+            return "";
+        }
+        if(word.startsWith("apple")){
+            return skipWord(word.substring(5));
+        }else{
+         return  word.charAt(0) + skipWord(word.substring(1));
         }
     }
 }
