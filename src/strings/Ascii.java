7d1a09ab3b6a5f310608e73fa7fb8ae7abbb9cc5
umeshbuilds <umenariya836@gmail.com>
2026-09-19 20:01:58 +0530

    ascii program changed

--- src/strings/Ascii.java
+++ src/strings/Ascii.java
@@ -5,12 +5,22 @@ import java.util.List;
 
 public class Ascii {
     static void main(String[] args) {
+        System.out.println(subsetAscii("","abc"));
+    }
+    static ArrayList<String> subsetAscii(String p,String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+         char ch = up.charAt(0);
+        ArrayList<String> first = subsetAscii(p+ch,up.substring(1));
+        ArrayList<String> second = subsetAscii(p,up.substring(1));
+        ArrayList<String> third = subsetAscii(p+(ch+0),up.substring(1));
+        first.addAll(second);
+        first.addAll(third);
+        return first;
+    }
 
-    }
-    static List<List<Integer>> subset(int[] arr){
-         List<List<Integer>> outer = new ArrayList<>();
-         outer.add(new ArrayList<>());
-         return outer;
-    }
 
 }
